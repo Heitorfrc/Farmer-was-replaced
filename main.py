28da617ecaedbc7160ab681461ab5e8d2958fa73
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2026-02-23 17:42:25 -0300

    Add cactus planting logic to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # projeto em andamento
+
 # perfumarias
 change_hat(Hats.Pumpkin_Hat)
 pet_the_piggy()
 do_a_flip()
+
 
 # arar o terreno
 def arar() :
@@ -61,6 +63,12 @@ a = 0
 medida_abobora = 10
 area_abobora = medida_abobora * medida_abobora
 
+# valores para a plantacao de cactus
+cac = False
+medida_cactus = 5
+posicao_inicial_x_cactus = 0
+posicao_inicial_y_cactus = 11
+
 # quando emular uma fazendo nova tira o # antes de clear() e de arar()
 clear()
 arar() 
@@ -71,14 +79,16 @@ while(True) :
 		
 		# colher
 		if get_pos_x() == 0 and get_pos_y() == 0 :
-			a = 0
-		
+			a = 0   # posiciao inicial da abobora gigante
+			
 		if can_harvest() or get_entity_type() == Entities.Dead_Pumpkin :
-			if get_entity_type() != Entities.Pumpkin :
+			if get_entity_type() != Entities.Pumpkin and get_entity_type() != Entities.Cactus :
 				harvest()
 			if get_entity_type() == Entities.Pumpkin :
 				a += 1
 			if a == area_abobora:
+				harvest()
+			if get_pos_x() == medida_cactus - 1 and get_pos_y() == medida_cactus - 1 and cac == True :
 				harvest()
 		
 		# plantar
@@ -90,12 +100,30 @@ while(True) :
 			if (get_pos_y() < medida_abobora and get_pos_x() < medida_abobora) :
 				plantar_aboboras()
 						
-			elif get_pos_y() >= 13 and get_pos_x() <= 2 :
+			elif get_pos_x() >= posicao_inicial_x_cactus and get_pos_x() < posicao_inicial_x_cactus + medida_cactus and get_pos_y() >= posicao_inicial_y_cactus and get_pos_y() < posicao_inicial_y_cactus + medida_cactus :
+				
+				if get_entity_type() == Entities.Cactus :
+					if get_pos_x() == posicao_inicial_x_cactus and get_pos_y() == posicao_inicial_y_cactus :
+						cac = True
+					if get_pos_y() < posicao_inicial_y_cactus + medida_cactus - 2 :
+						if measure() > measure (North) :
+							swap(North)
+							cac = False
+					if get_pos_y() == posicao_inicial_y_cactus + medida_cactus - 1 :
+						if measure() < measure(South) :
+							swap(South)
+							cac = False
+					if get_pos_x() < posicao_inicial_x_cactus + medida_cactus - 2 :
+						if measure() > measure (East) :
+							swap(East)
+							cac = False
+					if get_pos_x() == posicao_inicial_x_cactus + medida_cactus - 1 :
+						if measure() < measure (West) :
+							swap(West)
+							cac = False
+				
 				plant(Entities.Cactus)
 				
-			elif get_pos_y() >= 13 and get_pos_x() >= 4 and get_pos_x() <= 6 :
-				plant(Entities.Cactus)
-			
 			else :
 				plantar_madeiras()
 			
